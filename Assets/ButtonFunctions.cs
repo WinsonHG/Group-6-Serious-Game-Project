ec29f33f3ec7f55a96086ebd5f1651f18f464e16
Mohammad5566 <mohammadmahmud5566@yahoo.com>
2022-04-13 18:22:34 -0400

    added Menu screen with play game button

---
+++ Assets/ButtonFunctions.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame() {
+        SceneManager.LoadScene("level1");
+    }
+
+    public void MainMenu() {
+        SceneManager.LoadScene("Menu");
+    }
+}
